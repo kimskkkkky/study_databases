6c29e04ddcc9ebf09c2a9b4cff207d80d744eda4
kimskkkkky <ses513a@gmail.com>
2023-06-07 12:17:02 +0900

    실습

---
+++ Members/Memers_Deletes.sql
@@ -0,0 +1,10 @@
+-- DELETE FROM table_name WHERE condition;
+
+
+
+DELETE FROM members_hobby ;
+DELETE FROM membewrs_login ;
+DELETE FROM hobby_code ;
+DELETE FROM members ;
+DELETE FROM address_code ;
+DELETE FROM gender_code ;
