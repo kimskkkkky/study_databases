c68770fb42ed257dcb191c41072f768564c1e1af
kimskkkkky <ses513a@gmail.com>
2023-06-07 12:19:57 +0900

    수정

--- Members/Memers_Deletes.sql
+++ Members/Memers_Deletes.sql
@@ -1,7 +1,8 @@
 -- DELETE FROM table_name WHERE condition;
 
 
-
+-- DB_MEMBERS의 모든 데이터를 삭제 한다.
+-- FOREIGN KEY 데이터를 먼저 삭제하고 PRIMARY KEY 데이터를 삭제한다.
 DELETE FROM members_hobby ;
 DELETE FROM membewrs_login ;
 DELETE FROM hobby_code ;
