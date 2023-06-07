2eff77d3e0dfd01b826db763578e2bec7dac24a8
kimskkkkky <ses513a@gmail.com>
2023-06-07 17:35:43 +0900

    수정

--- SQLS/whereOrderGroupby.sql
+++ SQLS/whereOrderGroupby.sql
@@ -1,11 +1,11 @@
 -- 조건 : EmployeeID가 7, 9 직원에 각각 주문 갯수
 -- 링크:https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_groupby
 
-SELECT COUNT(*)
+SELECT COUNT(*), EmployeeID
 FROM Orders
 WHERE EmployeeID IN (7,9)
 GROUP BY EmployeeID;
 
 -- Number of Records: 2
--- 14
--- 6
+-- ID 7 = 14 
+-- ID 9 = 6
