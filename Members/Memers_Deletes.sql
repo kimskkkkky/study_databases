b7ce82589750848e51536174f45f17ca47896369
kimskkkkky <ses513a@gmail.com>
2023-06-07 12:29:29 +0900

    수정

--- Members/Memers_Deletes.sql
+++ Members/Memers_Deletes.sql
@@ -4,7 +4,7 @@
 -- DB_MEMBERS의 모든 데이터를 삭제 한다.
 -- FOREIGN KEY 데이터를 먼저 삭제하고 PRIMARY KEY 데이터를 삭제한다.
 DELETE FROM members_hobby ;
-DELETE FROM membewrs_login ;
+DELETE FROM members_login ;
 DELETE FROM hobby_code ;
 DELETE FROM members ;
 DELETE FROM address_code ;
