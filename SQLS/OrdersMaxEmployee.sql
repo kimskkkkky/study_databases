5b38c1417bdf364140b8095244f8630588fb6322
kimskkkkky <ses513a@gmail.com>
2023-06-08 16:28:14 +0900

    수정

--- SQLS/OrdersMaxEmployee.sql
+++ SQLS/OrdersMaxEmployee.sql
@@ -1,23 +1,18 @@
 -- 링크 : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all
 -- 조건 : Orders TABLE에서 가장 많이 주문 받은 회사 직원명과 갯수
-SELECT COUNT(FirstName) AS ORDER_COUNT, FirstName, ShipperName
-FROM (
-		Orders AS T_ORD
-		INNER JOIN Employees AS T_EMP
-		ON T_ORD.EmployeeID = T_EMP.EmployeeID)
-  	INNER JOIN Shippers AS T_SHP
-    ON T_ORD.ShipperID = T_SHP.ShipperID 
-GROUP BY FirstName;
+
+
+
+SELECT MAX(ORDER_COUNT), FirstName, LastName, ShipperName  -- 최대 주문량과 직원 이름, 회사이름
+FROM (SELECT COUNT(FirstName) AS ORDER_COUNT, FirstName, LastName, ShipperName  --직원 이름 별로 주문수량, 회사 이름 리스트
+       FROM (Orders AS T_ORD
+       		INNER JOIN Employees AS T_EMP
+       		ON T_ORD.EmployeeID = T_EMP.EmployeeID)
+        INNER JOIN Shippers AS T_SHP
+        ON T_ORD.ShipperID = T_SHP.ShipperID 
+       	GROUP BY FirstName);
 
 -- Result:
--- Number of Records: 9
--- ORDER_COUNT	LastName	FirstName	ShipperName
--- 20	         Fuller	     Andrew	     Speedy Express
--- 6	         Dodsworth	    Anne	Federal Shipping
--- 31	          Leverling	   Janet	 Speedy Express
--- 27	          Callahan	    Laura	 Federal Shipping
--- 40	          Peacock	   Margaret	  United Package
--- 18	          Suyama	  Michael	   Speedy Express
--- 29	           Davolio	    Nancy	 Speedy Express
--- 14	            King	    Robert	 Federal Shipping
--- 11	           Buchanan	    Steven	  Federal Shipping
+-- Number of Records: 1
+-- MAX(ORDER_COUNT)	FirstName	LastName	ShipperName
+-- 40	             Margaret	 Peacock	United Package
