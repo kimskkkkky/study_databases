bbdfe33b30e15f4a35830d4153686d543f768c89
kimskkkkky <ses513a@gmail.com>
2023-06-08 16:38:41 +0900

    수정

--- SQLS/OrdersMaxEmployee.sql
+++ SQLS/OrdersMaxEmployee.sql
@@ -3,14 +3,12 @@
 
 
 
-SELECT MAX(ORDER_COUNT), FirstName, LastName, ShipperName  -- 최대 주문량과 직원 이름, 운송회사 이름
-FROM (SELECT COUNT(FirstName) AS ORDER_COUNT, FirstName, LastName, ShipperName  --직원 이름 별로 주문수량, 운송회사 이름 리스트
-       FROM (Orders AS T_ORD
-       		INNER JOIN Employees AS T_EMP
-       		ON T_ORD.EmployeeID = T_EMP.EmployeeID)
-        INNER JOIN Shippers AS T_SHP
-        ON T_ORD.ShipperID = T_SHP.ShipperID 
-       	GROUP BY FirstName);
+SELECT MAX(ORDER_COUNT), LastName, FirstName -- 최대 주문량과 직원 이름
+FROM (SELECT COUNT(FirstName) AS ORDER_COUNT, LastName, FirstName  --직원 이름 별로 주문수량 리스트
+       FROM Orders AS T_ORD
+        INNER JOIN Employees AS T_EMP
+       	ON T_ORD.EmployeeID = T_EMP.EmployeeID
+        GROUP BY FirstName);
 
 -- Result:
 -- Number of Records: 1
