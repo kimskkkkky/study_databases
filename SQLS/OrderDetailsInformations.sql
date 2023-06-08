ffc65cbf6822861faf3109d2386ce6414127f6cf
kimskkkkky <ses513a@gmail.com>
2023-06-08 18:18:07 +0900

    실습

---
+++ SQLS/OrderDetailsInformations.sql
@@ -0,0 +1,14 @@
+-- 링크 : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all
+-- 조건 : 
+
+SELECT  ProductName, Price, Quantity, CustomerName
+FROM ((OrderDetails AS ORDDT
+			INNER JOIN Products AS PRD   -- OrderDetails과 Products를 JOIN해서 상품명, 상품 가격, 주문 갯수 출력 / OR_PRD 테이블 생성됨.
+			ON ORDDT.ProductID = PRD.ProductID) AS OR_PRD
+		INNER JOIN Orders AS ORD         -- OR_PRD와 Orders JOIN 해서 회원 아이디 출력 / OR_PRD_C 테이블이 생성됨
+		ON OR_PRD. OrderID = ORD.OrderID) AS OR_PRD_C
+INNER JOIN Customers AS CUST    -- OR_PRD_C와 Customers JOIN 회원 이름 출력
+ON OR_PRD_C.CustomerID = CUST.CustomerID
+
+-- Result:
+-- Number of Records: 518
